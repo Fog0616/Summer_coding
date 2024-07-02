c8e536eea3d93a8509f2e0535d6831b42263983a
Cuiti <cuiti708@gmail.com>
2024-07-02 11:11:21 +0800

    P2670 and Better p2670

---
+++ P2670.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int dx[]={1,1,1,0,0,-1,-1,-1};
+const int dy[]={-1,0,1,-1,1,-1,0,1};
+const int maxn=105;
+char g[maxn][maxn];
+int n,m;
+int main(){
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>g[i][j];
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(g[i][j]!='*'){
+                int cnt = 0;
+                for(int k=0;k<8;k++)
+                    if(g[i+dx[k]][j+dy[k]]=='*') cnt++;
+                cout<<cnt;
+
+            }
+            else cout<<'*';
+        
+        }
+        cout<<endl;
+    }
+    return 0;
+}
